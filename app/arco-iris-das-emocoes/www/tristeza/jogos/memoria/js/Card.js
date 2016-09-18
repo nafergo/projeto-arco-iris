ece882599f8aad37ef8a8505bd644954283be1e1
Maria Pereira <mariafpereiram@gmail.com>
2016-09-18 03:07:16 +0100

    tristeza
    alteração das imagens correspondentes à tristeza

---
+++ app/arco-iris-das-emocoes/www/tristeza/jogos/memoria/js/Card.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Author: Maximo Mena
+ * GitHub: mmenavas
+ * Twitter: @menamaximo
+ * Project: Memory Workout
+ * Description: A JS, HTML and CSS based memory game.
+ * The goal is to match pairs of cards in the least
+ * number of matching attempts.
+ */
+
+/**
+ * @namespace Card object
+ */
+MemoryGame.Card = function(value) {
+  this.value = value;
+  this.isRevealed = false;
+
+  this.reveal = function() {
+    this.isRevealed = true;
+  }
+
+  this.conceal = function() {
+    this.isRevealed = false;
+  }
+};
